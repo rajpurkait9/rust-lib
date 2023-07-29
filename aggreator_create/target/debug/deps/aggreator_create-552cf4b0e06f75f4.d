869da2136577e1aebe4dbb84bb153ad72d861814
raj purkait <rajpurkait9@gmail.com>
2023-07-29 23:58:01 +0530

    initila commit

---
+++ aggreator_create/target/debug/deps/aggreator_create-552cf4b0e06f75f4.d
@@ -0,0 +1,6 @@
+/home/raj-purkait/Desktop/learn-rust/aggreator_create/target/debug/deps/aggreator_create-552cf4b0e06f75f4.rmeta: src/lib.rs src/aggreator.rs
+
+/home/raj-purkait/Desktop/learn-rust/aggreator_create/target/debug/deps/aggreator_create-552cf4b0e06f75f4.d: src/lib.rs src/aggreator.rs
+
+src/lib.rs:
+src/aggreator.rs:
---
+++ aggreator_create/target/debug/libaggreator_create.d
@@ -0,0 +1,1 @@
+/home/raj-purkait/Desktop/learn-rust/aggreator_create/target/debug/libaggreator_create.rlib: /home/raj-purkait/Desktop/learn-rust/aggreator_create/src/aggreator.rs /home/raj-purkait/Desktop/learn-rust/aggreator_create/src/lib.rs
